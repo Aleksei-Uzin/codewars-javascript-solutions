f2125304d1aa0675103cf2f60062f9d1186090ec
Aleksei-Uzin <aleksei.uzin@gmail.com>
2026-05-21 14:27:49 +0300

    5 kyu: Replicate new

---
+++ OOP/5-kyu-Replicate-new.js
@@ -0,0 +1,22 @@
+/**
+ * Replicate `new`
+ * https://www.codewars.com/kata/558cb3df5f511f40d500001d
+ *
+ */
+
+function nouveau(Constructor, ...args) {
+  const instance = Object.create(Constructor.prototype)
+  const newObj = Constructor.call(instance, ...args)
+  const isObject = newObj && (typeof newObj === 'object' || typeof newObj === 'function')
+
+  return isObject ? newObj : instance
+}
+
+/**
+ * Best Practice
+ */
+function nouveau(Constructor, ...args) {
+  const instance = Object.create(Constructor.prototype)
+  const result = Constructor.apply(instance, args)
+  return result === Object(result) ? result : instance
+}
